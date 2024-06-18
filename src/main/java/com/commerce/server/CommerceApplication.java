96592ad32d2d5315dd0edefaa9578ecd6a1cd41c
dc-choi <ddagae0805@gmail.com>
2024-06-18 20:16:55 +0900

    chore: 초기 프로젝트 세팅

---
+++ src/main/java/com/commerce/server/CommerceApplication.java
@@ -0,0 +1,13 @@
+package com.commerce.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class CommerceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceApplication.class, args);
+    }
+}
